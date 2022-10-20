725f48c60138b8a0eff8c651f2166eabc654b8fc
pranab <sarker.pranab@genweb2.com>
2022-10-20 19:41:44 +0600

    Navigation styled to BS5

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,33 +16,42 @@ const Nav = () => {
         navigate('/')
     }
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">Annotation</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+            <div class="container-fluid">
+                <Link className="navbar-brand fw-bold" to="/">Annotation</Link>
+                
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <div className="ml-auto">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     {
                         IsLoggedIn ?
                             <>
-                                <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/videos">Videos</Link></button>
-                                <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/category">Category</Link></button>
-                                <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/complete">Complete</Link></button>
-                                <button onClick={logoutHandler} className="btn btn-outline-primary mr-1 auth-btn">Log out</button> 
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <Link class="nav-link" to="/videos">Videos</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link class="nav-link" to="/category">Category</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link class="nav-link" to="/complete">Complete</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <button onClick={logoutHandler} className="btn btn-link auth-btn">Log out</button> 
+                                    </li>
+                                </ul>
                             </>:
                             pathname === '/signin' ?
-                                <button className="btn btn-outline-primary auth-btn"><Link to="/signup">Sign-up</Link></button> :
-                                pathname === '/signup' ?
-                                    <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/signin">Sign-in</Link></button> :
-                                    <>
-                                        <button className="btn btn-outline-primary auth-btn"><Link to="/signup">Sign-up</Link></button>
-                                        <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/signin">Sign-in</Link></button>
-                                    </>
+                                <button className="btn btn-outline-primary auth-btn"><Link to="/signup">Sign-up</Link></button> : pathname === '/signup' ?
+                                <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/signin">Sign-in</Link></button> :
+                                <>
+                                    <button className="btn btn-outline-primary auth-btn"><Link to="/signup">Sign-up</Link></button>
+                                    <button className="btn btn-outline-primary mr-1 auth-btn"><Link to="/signin">Sign-in</Link></button>
+                                </>
                     }
-
                 </div>
             </div>
         </nav>
